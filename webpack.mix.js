86bbec24a9b80490885d7f36396bf34683cd2f9c
fahrulazmi <fahrulazmi@gmail.com>
2021-02-10 03:46:14 +0800

    include source maps

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,13 @@ const mix = require('laravel-mix');
 mix.js('src/js/app.js', 'public/js')
    .sass('src/sass/app.scss', 'public/css');
 
+if (!mix.inProduction()) {
+    mix.sourceMaps()
+    mix.webpackConfig({
+        devtool: 'inline-source-map'
+    })
+}
+
 mix.browserSync({
     host: 'localhost',
     port: 3000,
